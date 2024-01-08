2a3519569e7f63fb369b1aa547413dd464770596
Rodrigo <rodrigomartins@id.uff.br>
2024-01-07 22:05:08 -0300

    It was added where the data was retrieved, it was removed the data filter and it was expanded the data filter. Some data other manipulation was added as well

--- main.py
+++ main.py
@@ -4,18 +4,9 @@ from pySankey.sankey import sankey
 import matplotlib.pyplot as plt
 
 # Load data and filter
+# http://comexstat.mdic.gov.br/pt/geral/100572
 df = pd.read_excel("data.xlsx")
-df =df[df["Descrição SH6"].isin(
-["Soja, mesmo triturada, exceto para semeadura",
- "Óleos brutos de petróleo ou de minerais betuminosos",
- "Minérios de ferro não aglomerados e seus concentrados",
- "Carnes de bovino, desossadas, congeladas",
- "Milho, exceto para semeadura",
-"Tortas e outros resíduos sólidos da extração do óleo de soja",
-"Café não torrado, não descafeinado"
-"Pasta química de madeira de não conífera, à soda ou sulfato, semibranqueada ou branqueada"
-])]
-df = df.head(120)
+df = df.head(200)
 df = df.replace({"Soja, mesmo triturada, exceto para semeadura": "Saca de soja",
                  "Óleos brutos de petróleo ou de minerais betuminosos": "Barril de petróleo",
                  "Minérios de ferro não aglomerados e seus concentrados": "Minério de ferro",
@@ -26,7 +17,10 @@ df = df.replace({"Soja, mesmo triturada, exceto para semeadura": "Saca de soja",
                  "Tortas e outros resíduos sólidos da extração do óleo de soja": "Resíduos de óleo de soja",
                  "Café não torrado, não descafeinado": "Saca de café",
                  "Pasta química de madeira de não conífera, à soda ou sulfato, semibranqueada ou branqueada": "Celulose",
+                 "Outros óleos de petróleo ou de minerais betuminosos e preparações, exceto desperdícios": "Derivados do petróleo",
+                 "Virgens, Ilhas (Americanas)": "Ilhas Virgens",
                  })
+
 # Create Sankey diagram again
 sankey(
     left=df["Descrição SH6"],
@@ -36,6 +30,7 @@ sankey(
     aspect=60,
     fontsize=12
 )
+
 # Get current figure
 fig = plt.gcf()
 
